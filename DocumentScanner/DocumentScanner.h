76c77c68e65ddbbbb8571ec0e558532c8df884dc
Waseem Akram <waseem.mohemmed@thomsonreuters.com>
2019-11-14 17:05:41 +0530

    Name-change-issues

--- DocumentScanner/DocumentScanner.h
+++ DocumentScanner/DocumentScanner.h
@@ -8,11 +8,11 @@
 
 #import <Foundation/Foundation.h>
 
-//! Project version number for Scan_Framework.
-FOUNDATION_EXPORT double Scan_FrameworkVersionNumber;
+//! Project version number for DocumentScanner
+FOUNDATION_EXPORT double DocumentScannerVersionNumber;
 
-//! Project version string for Scan_Framework.
-FOUNDATION_EXPORT const unsigned char Scan_FrameworkVersionString[];
+//! Project version string for DocumentScanner
+FOUNDATION_EXPORT const unsigned char DocumentScannerVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Scan_Framework/PublicHeader.h>
 
